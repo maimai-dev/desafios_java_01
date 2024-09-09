0ad11897cec2d68a84b6ef00d94270c93d6501cc
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2024-09-09 17:48:49 -0300

    commit 09/09

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main{
+//Desafio 02
+
+ class Main{
     public static void main(String [] args){
+        double media = (8.4 + 5.9) /3;
+        System.out.println(media);
+
+        double num1 = 4.5;
+        int num2 = 10;
+
+        int casting = (int) (num1);
+        System.out.println(casting);
+
+        char caractere = 'A';
+        String letras = "Dev";
+
+        System.out.println("A letra é: "+ caractere + " Profissão é: " + letras);
+
+        double precoProduto = 5.50;
+        int quantidade = 5;
+
+        double resultado = precoProduto * quantidade;
+        System.out.println(resultado);
 
     }
 }
